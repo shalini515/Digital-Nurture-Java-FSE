29b7282f801a25e53ace004bf1231453f9f435da
shalini <shalinivenkatesan2004@gmail.com>
2025-08-02 21:12:04 +0530

    Add week-6 task-2

---
+++ src/week6/student-app/src/App.js
@@ -0,0 +1,16 @@
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <About></About>
+      <Contact></Contact>
+    </div>
+  );
+}
+
+export default App;
